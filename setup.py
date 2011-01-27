a57e41bb84a0cfb5a3025d98c37037b2721cc96f
bitcraft <leif.theden@gmail.com>
2011-01-27 15:00:55 +0800

    merge with bravo

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="Bravo",
-    version="1.0",
-    packages=find_packages(),
+    version="1.1",
+    packages=[
+        "bravo",
+        "bravo.plugins",
+        "bravo.protocols",
+        "bravo.serializers",
+        "bravo.tests",
+        "twisted.plugins",
+    ],
     install_requires=[
         "numpy",
-        "reconstruct",
-        "Twisted",
+        "construct>=2.03",
+        "Twisted>=10",
     ],
     author="Corbin Simpson",
     author_email="MostAwesomeDude@gmail.com",
@@ -17,9 +24,10 @@ setup(
     long_description=open("README.rst").read(),
     license="MIT/X11",
     url="http://github.com/MostAwesomeDude/bravo",
-    entry_points={
-        "console_scripts": [
-            "bravo = bravo.main:main",
-        ],
-    },
 )
+
+try:
+    from twisted.plugins import getPlugins, IPlugin
+    list(getPlugins(IPlugin))
+except:
+    pass
